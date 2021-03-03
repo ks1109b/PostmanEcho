ae15aafd841b18562ad6a258a07fde279e5434ef
Kseniia Bloshkina <ksenia119@hotmail.com>
2021-03-03 22:30:56 +0300

    Failed build

--- src/test/java/ru/netology/rest/PostmanTest.java
+++ src/test/java/ru/netology/rest/PostmanTest.java
@@ -1,5 +1,6 @@
 package ru.netology.rest;
 
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -7,11 +8,12 @@ import static org.hamcrest.Matchers.*;
 
 class PostmanTest {
     @Test
-    void should() {
+    void shouldGetWhatSend() {
         // Given - When - Then
         // Предусловия
         given()
                 .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
                 .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
                 // Выполняемые действия
                 .when()
@@ -19,7 +21,8 @@ class PostmanTest {
                 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"))
+                .body("data", equalTo("some value"))
+                .contentType(ContentType.JSON)
         ;
     }
 }
